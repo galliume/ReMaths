0346e6e06a3b115a58af9d4cd94a9d9871a12b1b
Guillaume P <gcconantc@gmail.com>
2022-01-27 18:41:11 +0100

    make it work for windows

--- premake5.lua
+++ premake5.lua
@@ -10,23 +10,25 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 IncludeDir = {}
 IncludeDir["googletest"] = "vendor/googletest/googletest"
-IncludeDir["googlemock"] = "vendor/googletest/googlemock"
 
 project "GoogleTest"
     kind "StaticLib"
     files 
     {
         "%{IncludeDir.googletest}/src/gtest-all.cc",
-        "%{IncludeDir.googlemock}/src/gmock-all.cc" 
     }
 
     includedirs 
     { 
         "%{IncludeDir.googletest}/include", 
-        "%{IncludeDir.googletest}",
-        "%{IncludeDir.googlemock}/include", 
-        "%{IncludeDir.googlemock}"
+        "%{IncludeDir.googletest}"        
     }
+
+    filter { "system:windows", "configurations:Debug"}
+        systemversion "latest"
+        runtime "Debug"
+        symbols "on"
+        buildoptions { "/MTd" }
 
 project "ReMaths"
     location ""
@@ -49,7 +51,6 @@ project "ReMaths"
     {
         "include",
         "%{IncludeDir.googletest}/include",
-        "%{IncludeDir.googlemock}/include"
     }
 
     links
